d62c57ad9cfa1c78e9dc88c2f796e9563c74f9f2
Benjamin Priestman <benjamin@miniverse.me.uk>
2022-03-08 06:39:45 +0000

    feat: add a cow via git

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var cowsay = require("cowsay");
+const cowsayTag = require('cowsay-tag');
 
 console.log(cowsay.say({
     text : "I'm a moooodule",
     e : "oO",
     T : "U "
 }));
+
+console.log(cowsayTag`And another thing..`)
